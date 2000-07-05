954a916c27d3dd202b02936dcca9b03551536df4
mitch <mitch>
2000-07-05 13:49:02 +0000

    Added leftblock.pl and rightblock.pl: Both are just showing a message; programs do nothing yet.

---
+++ tools/leftblock.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl -w
+use strict;
+print "leftblock.pl:\nThis tool will be included in a later version.\nSorry for now...\n";
+exit -1;
---
+++ tools/rightblock.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl -w
+use strict;
+print "rightblock.pl:\nThis tool will be included in a later version.\nSorry for now...\n";
+exit -1;
