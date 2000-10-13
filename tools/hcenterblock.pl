61c92220c659b9c22d1029c47d0cce66c2a77258
mitch <mitch>
2000-10-13 16:37:55 +0000

    Funktioniert jetzt

--- tools/hcenterblock.pl
+++ tools/hcenterblock.pl
@@ -1,4 +1,58 @@
 #!/usr/bin/perl -w
+#
+# $Revision: 1.3 $
+#
+# 2000 (C) by Christian Garbs <mitch@uni.de>
+# centers a block horizontally
+
 use strict;
-print "hcenterblock.pl:\nThis tool will be included in a later version.\nSorry for now...\n";
-exit -1;
+
+my ($width, $file, @data, $longest);
+
+$width   =  79 unless ($width = shift);
+$file    = "-" unless ($file  = shift);
+$longest =   0;
+
+if (($width eq "--help") or ($file eq "--help")) {
+
+    print "Usage:\n";
+    print "  hcenterblock.pl [width] [file1] [file2] [file3] [...]\n";
+    print "This program will center a given block horizontally\n";
+
+    exit 0;
+}
+
+die "width is not numeric!\n" unless $width =~ /^\+?\d*$/;
+
+input($file);
+
+foreach $file (@ARGV) {
+    input($file);
+}
+
+center();
+
+exit 0;
+
+sub input()
+{
+    my $filename = $_[0];
+    
+    open FILE, "$filename" or die "can't read $filename: $!";
+    
+    while (my $line = <FILE>) {
+	chomp $line;
+	if (length $line > $longest) {
+	    $longest = length $line;
+	}
+	push @data, $line;
+    }
+}
+
+sub center()
+{
+    my $spacestoadd = " " x (($width - $longest) / 2);
+    foreach my $line (@data) {
+	print "$spacestoadd$line\n";
+    }
+}
--- tools/leftblock.pl
+++ tools/leftblock.pl
@@ -1,4 +1,64 @@
 #!/usr/bin/perl -w
+#
+# $Revision: 1.2 $
+#
+# 2000 (C) by Christian Garbs <mitch@uni.de>
+# centers a block horizontally
+
 use strict;
-print "leftblock.pl:\nThis tool will be included in a later version.\nSorry for now...\n";
-exit -1;
+
+my ($file, @data, $cutoff);
+
+$file = "-" unless ($file = shift());
+
+if ($file eq "--help") {
+
+    print "Usage:\n";
+    print "  leftblock.pl [file1] [file2] [file3] [...]\n";
+    print "This program will align a given block to the left\n";
+
+    exit 0;
+
+}
+
+input($file);
+
+foreach $file (@ARGV) {
+    input($file);
+}
+
+$cutoff = 0 unless $cutoff;
+
+left();
+
+exit 0;
+
+sub input()
+{
+    my $filename = $_[0];
+    
+    open FILE, "$filename" or die "can't read $filename: $!";
+    
+    while (my $line = <FILE>) {
+	chomp $line;
+	
+	if ($line =~ /( +)/) {
+	    if ($cutoff) {
+		if ($cutoff > length $1) {
+		    $cutoff = length $1;
+		}
+	    } else {
+		$cutoff = length $1;
+	    }
+	}
+	push @data, $line;
+    }
+}
+
+sub left()
+{
+    foreach my $line (@data) {
+	$line = substr $line, $cutoff;
+	print "$line\n";
+    }
+}
