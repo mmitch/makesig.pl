ef31adbb6eefe4c8108e670f2b0d029196fabd26
mitch <mitch>
2000-10-13 16:51:06 +0000

    Suchpfaderweiterung eingebaut Suchpfaderweiterung nach vorne gesetzt, damit nicht die Tools der letzten installierten Version angezogen werden.

--- examples/tools.sh
+++ examples/tools.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 #
+# $Revision: 1.3 $
+#
 # NOTE:
-# These examples want the tools to reached by ../tools/ and
-# makesig.pl to be found in ./../
-# Should be no problem if you untared the archive as usual
+# These examples want the tools to be present in the $PATH
+# ./../ and ./../tools/ are added to the $PATH
+# This should alright if you untared the archive as usual
 # and now are in the examples directory.
 #
+
+PATH=../tools/:..:$PATH
+
+echo "This is a demonstration of the makesig.pl random signature generator."
+echo "Get your up-to-date copy at http://www.h.shuttle.de/mitch/makesig_pl.en.html"
+echo
 
 echo "Let's try working with static files."
 echo "Watch our demo text:"
@@ -13,52 +21,48 @@ echo
 
 cat demo.txt
 
-
-
 echo 
 echo "Now let's center it:"
 echo 
 
-../tools/hcenter.pl < demo.txt
+hcenter.pl < demo.txt
 
 echo 
 echo "See the difference when it is centered as a block:"
 echo 
 
-../tools/hcenterblock.pl < demo.txt
-
-
-
-echo
-echo "Static files are boring. Here's your uptime:"
-
-/usr/bin/uptime
-
-echo
-echo "Now lets put that uptime to the right:"
-
-/usr/bin/uptime \
-| ../tools/right.pl
+hcenterblock.pl < demo.txt
 
 
 
 echo
 echo "Now we put the demo text on top of a background:" 
 
-../tools/overlay.pl demo.txt background.txt
+overlay.pl demo.txt background.txt
 
 echo
 echo "Now we put the background on top of the demo text :-)"
 
-../tools/overlay.pl background.txt demo.txt
+overlay.pl background.txt demo.txt
 
 echo
 echo "Bottom right on a background:"
 
-../tools/bottom.pl < demo.txt \
-| ../tools/right.pl \
-| ../tools/overlay.pl - background.txt
+bottom.pl < demo.txt \
+| right.pl \
+| overlay.pl - background.txt
 
+
+
+echo
+echo "Static files are boring. Here's your uptime:"
+
+uptime
+
+echo
+echo "Now lets put that uptime to the right:"
+
+uptime | right.pl
 
 
 
@@ -72,30 +76,30 @@ echo
 TEMP=/tmp/`basename $0`-$$
 
 # this is the left part (ascii graphics)
-./../makesig.pl ./left.conf \
-| ./../tools/bottom.pl 4 > $TEMP.left
+makesig.pl left.conf \
+| bottom.pl 4 > $TEMP.left
 
 # this is the right side
 # top (quote)
-./../makesig.pl ./right.conf \
-| ./../tools/left.pl \
-| /usr/bin/cut -c 1-66 \
-| ./../tools/top.pl 2 \
-| ./../tools/hcenter.pl 66 \
-| ./../tools/indent.pl 13 > $TEMP.right
+makesig.pl right.conf \
+| left.pl \
+| cut -c 1-66 \
+| top.pl 2 \
+| hcenter.pl 66 \
+| indent.pl 13 > $TEMP.right
 
 # bottom (uptime)
-/usr/bin/uptime \
-| ./../tools/bottom.pl 2 \
-| /usr/bin/cut -c 1-66 \
-| ./../tools/right.pl 66 \
-| ./../tools/indent.pl 13 >> $TEMP.right
+uptime \
+| bottom.pl 2 \
+| cut -c 1-66 \
+| right.pl 66 \
+| indent.pl 13 >> $TEMP.right
 
 # sigdashes
 echo "-- "
 
 # put everything together
-./../tools/overlay.pl $TEMP.right $TEMP.left
+overlay.pl $TEMP.right $TEMP.left
 
 # clean up
 rm $TEMP.*
@@ -106,10 +110,10 @@ echo "files. Much cooler, but nobody can read the script :-)"
 echo
 
 echo "-- "
-./../tools/overlay.pl \
-"./../makesig.pl ./left.conf | ./../tools/bottom.pl 4 | " \
-"./../makesig.pl ./right.conf | ./../tools/left.pl | /usr/bin/cut -c 1-66 | ./../tools/top.pl 2 | ./../tools/hcenter.pl 66| ./../tools/indent.pl 13|" \
-"/usr/bin/uptime | ./../tools/bottom.pl 4 | /usr/bin/cut -c 1-66 | ./../tools/right.pl 66| ./../tools/indent.pl 13|"
+overlay.pl \
+"makesig.pl left.conf | bottom.pl 4 | " \
+"makesig.pl right.conf | left.pl | cut -c 1-66 | top.pl 2 | hcenter.pl 66| indent.pl 13|" \
+"/usr/bin/uptime | bottom.pl 4 | cut -c 1-66 | right.pl 66| indent.pl 13|"
 
 
 
