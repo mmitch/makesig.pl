9fb8b27f8d789215fc97abb439d64cc103de664f
mitch <mitch>
2000-10-17 20:09:36 +0000

    Bis auf readFile() fertig (?)

--- Makesig/File.pm
+++ Makesig/File.pm
@@ -12,13 +12,52 @@ sub new {
     my $self  = {};
     $self->{FILENAME}      = "";
     $self->{QUOTES}        = "";
-    if ($parent = shift) {
+    if (my $parent = shift) {
 	$self->{CONFIGURATION} = Configuration->new($parent->CONFIGURATION);
     } else {
 	$self->{CONFIGURATION} = Configuration->new();
     }
     bless ($self, $class);
     return $self;
+
+}
+
+##
+## attributes
+##
+
+sub filename {
+    my $self = shift;
+    if (@_) { $self->{FILENAME} = shift }
+    return $self->{FILENAME};
+}
+
+sub configuration {
+    my $self = shift;
+    if (@_) { $self->{CONFIGURATION} = shift }
+    return $self->{CONFIGURATION};
+}
+
+##
+## methods
+##
+
+sub getQuoteCount {
+    my $self = shift;
+    return scalar @{$self->{QUOTES}};
+}
+
+sub getQuote {
+    my $self = shift;
+    if (@_) {
+	return $self->{QUOTES}[shift];
+    }
+}
+
+sub readFile {
+    my $self = shift;
+
+    # INSERT CODE HERE
 
 }
 
