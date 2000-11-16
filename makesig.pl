907eb2a3752be8999d41206e8f68ec2a4dc18dbe
mitch <mitch>
2000-11-16 21:53:41 +0000

    =>v0.0.7

--- makesig.pl
+++ makesig.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-# $Revision: 1.9.6.5 $
+# $Revision: 1.9.6.6 $
 #
 # 2000 (C) by Christian Garbs <mitch@uni.de>
 #
@@ -11,7 +11,7 @@
 use strict;
 
 # version information
-my $version = "0.0.6  -  2000-11-12";
+my $version = "0.0.7  -  2000-11-16";
 
 sub read_file ($$);
 sub pick_quote();
